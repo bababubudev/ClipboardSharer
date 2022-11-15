981894e7949935dd565bec68f82f70bf781724c7
Dada DaiDon <prwshshrm@gmail.com>
2022-11-16 00:21:20 +0200

    Added flash!

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "daidonprabeshdada"
-app.permanent_session_lifetime = timedelta(hours=1)
+app.permanent_session_lifetime = timedelta(days=1)
+
+
+def user_in(current_session: dict()) -> bool:
+    if "user" in current_session:
+        return True
+    else:
+        return False
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == "POST":
         session.permanent = True
-        user = request.form["title"]
+        if user_in(session):
+            logout()
 
+        user = request.form["title"]
         session["user"] = user
 
         return redirect(url_for("inbox"))
     else:
-        if "user" in session:
+        if user_in(session):
             user = session["user"]
             return render_template('base.html', user_valid=user)
         else:
@@ -25,7 +34,7 @@ def index():
 
 @app.route('/user')
 def inbox():
-    if "user" in session:
+    if user_in(session):
         user = session["user"]
         return render_template('customarea.html', username=user)
     else:
@@ -34,7 +43,9 @@ def inbox():
 
 @app.route('/logout')
 def logout():
+    user = session["user"].capitalize()
     session.pop("user", None)
+    flash(f"{user} was replaced from the session!", "info")
 
     return redirect(url_for("index"))
 
