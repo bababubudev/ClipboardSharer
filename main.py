aeb43ef8bbf66291d00cb8f9e32229e6c9b880a7
Dada DaiDon <prwshshrm@gmail.com>
2022-12-10 02:30:50 +0200

    Finished with the project
    Bugs and workarounds are to be fixed though :(

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-from datetime import timedelta
+from datetime import timedelta, datetime
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import dotenv_values
+
 
 app = Flask(__name__)
-app.secret_key = "daidonprabeshdada"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///redirects.sqlite3'
+
+sutukka = dotenv_values(".env")
+app.secret_key = sutukka["secret_key"]
+user, password = sutukka["user"], sutukka["password"]
+host = sutukka["host"]
+db = sutukka["db"]
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username=user, password=password, hostname=host, databasename=db)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///redirects.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.permanent_session_lifetime = timedelta(days=1)
 
 db = SQLAlchemy(app)
@@ -14,15 +27,17 @@ db = SQLAlchemy(app)
 class redirects(db.Model):
     _id = db.Column("id", db.INTEGER, primary_key=True)
     linker = db.Column(db.String(30))
-    text = db.Column(db.String(500))
+    text = db.Column(db.String(320))
+    time_created = db.Column(db.DateTime)
 
-    def __init__(self, linker, text):
+    def __init__(self, linker, text, time_created):
         self.linker = linker
         self.text = text
+        self.time_created = time_created
 
 
-def redirect_of(current_session: dict()) -> bool:
-    if "link" in current_session:
+def redirect_of(current_session: dict(), key: str) -> bool:
+    if key in current_session:
         return True
     else:
         return False
@@ -30,27 +45,28 @@ def redirect_of(current_session: dict()) -> bool:
 
 @app.route('/', methods=["POST", "GET"])
 def index():
+    data_timeout()
     if request.method == "POST":
         session.permanent = True
         link = request.form["title"]
         link = " ".join(link.split())
 
-        if redirect_of(session) and link != session["link"]:
+        if redirect_of(session, "link") and link != session["link"]:
             logout()
 
         session["link"] = link
-
         found_user = redirects.query.filter_by(linker=link).first()
+
         if found_user:
             session["textinput"] = found_user.text
         else:
-            lnk = redirects(link, "")
+            lnk = redirects(link, "", datetime.now())
             db.session.add(lnk)
             db.session.commit()
 
         return redirect(url_for("inbox"))
     else:
-        if redirect_of(session):
+        if redirect_of(session, "link"):
             link = session["link"]
             return render_template('base.html', link_valid=link)
         else:
@@ -60,7 +76,7 @@ def index():
 @app.route('/link', methods=["POST", "GET"])
 def inbox():
     text = None
-    if redirect_of(session):
+    if redirect_of(session, "link"):
         link = session["link"]
 
         if request.method == "POST":
@@ -86,6 +102,21 @@ def logout():
     flash(f"{link.capitalize()} was replaced from the session!", "info")
 
     return redirect(url_for("index"))
+
+
+def data_timeout():
+    users = redirects.query.all()
+    # rows = session.query(redirects).all()
+
+    for user in users:
+        stoppage = datetime.now()
+        elapsed = stoppage - user.time_created
+
+        if elapsed > app.permanent_session_lifetime:
+            db.session.delete(user)
+            db.session.commit()
+            print(
+                f"Deleting user {user.linker} after {elapsed.total_seconds()}")
 
 
 if __name__ == "__main__":
